7208114c159dd718e8d22894a873b31347d860dc
hanfangyi07 <hanfangyi07@gmail.com>
2026-05-24 19:41:44 +0800

    Update bit.cpp

--- bit.cpp
+++ bit.cpp
@@ -28,37 +28,28 @@ long long countInversionsBIT(int* a, int n) {
 
 int main() {
     ifstream in("rankings50k.txt");
-    if (!in) { cerr << "Cannot open rankings.txt" << endl; return 1; }
     int n;
     in >> n;
-
     int* myRank = new int[n];
     for (int i = 0; i < n; i++) in >> myRank[i];
-
     int* pos = new int[n + 1];
     for (int i = 0; i < n; i++) pos[myRank[i]] = i;
-
     int* friendRank = new int[n];
     int* mapped = new int[n];
     int bestFriend = -1;
     double bestSim = -1.0;
     int friendId = 1;
-
-    long long total_algo_time = 0;   // 累加核心算法耗时（毫秒）
-
+    long long total_algo_time = 0;   // 耗时
     while (in >> friendRank[0]) {
         for (int i = 1; i < n; i++) in >> friendRank[i];
         for (int i = 0; i < n; i++) mapped[i] = pos[friendRank[i]] + 1;
-
         auto start = high_resolution_clock::now();
         long long inv = countInversionsBIT(mapped, n);
         auto end = high_resolution_clock::now();
         auto duration = duration_cast<milliseconds>(end - start);
         total_algo_time += duration.count();
-
         long long maxInv = (long long)n * (n - 1) / 2;
         double sim = (1.0 - (double)inv / maxInv) * 100.0;
-
         if (sim > bestSim) {
             bestSim = sim;
             bestFriend = friendId;
@@ -66,14 +57,8 @@ int main() {
         friendId++;
     }
 
-    if (bestFriend != -1) {
-        cout << "Best friend: friend " << bestFriend
-            << ", similarity = " << fixed << setprecision(2) << bestSim << "%" << endl;
-    }
-    else {
-        cout << "No friend data found." << endl;
-    }
-    cout << "Total algorithm time (BIT): " << total_algo_time << " ms" << endl;
+cout << "Best friend: friend " << bestFriend << ", similarity = " << fixed << setprecision(2) << bestSim << "%" << endl;
+cout << "Total algorithm time (BIT): " << total_algo_time << " ms" << endl;
 
     delete[] myRank; delete[] pos; delete[] friendRank; delete[] mapped;
     return 0;
